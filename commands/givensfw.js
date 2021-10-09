b890e03dca8c200ad5e83fd40cc4058b0d154706
ChenzIsHomo <88568085+ChenzIsHomo@users.noreply.github.com>
2021-10-09 17:50:31 +0100

    a

--- commands/givensfw.js
+++ commands/givensfw.js
@@ -1,9 +1,8 @@
-
 module.exports = {
 	name: 'givensfw',
 	description: 'ball',
 	execute(client, message, args) {
-        let role = message.guild.roles.cache.find(r => r.id === "831968147924975659");
+        let role = message.guild.roles.find(r => r.name === "NSFW");
         message.author.roles.add(role);
 	},
 };
