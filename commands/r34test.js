f6730af89f6672819e9d75041b8be6d4d6b19233
ChenzIsHomo <88568085+ChenzIsHomo@users.noreply.github.com>
2021-10-09 16:20:18 +0100

    Update r34test.js

--- commands/r34test.js
+++ commands/r34test.js
@@ -4,7 +4,7 @@ module.exports = {
     execute(client, message, args){
         if (!message.guild) return;
         if (message.channel.nsfw === false) return message.channel.send('wee wee woo woo this aint a poo poo channel')
-        import { posts } from "rule34js" // const posts = require("rule34js").posts
+        const { posts } = require('rule34js') // const posts = require("rule34js").posts
         const response = await posts({tags:["furry"]})
         console.log(response)
     }
