f8860115438902b2a83e3a4605e107aff5d50039
ChenzIsHomo <88568085+ChenzIsHomo@users.noreply.github.com>
2021-10-09 19:24:25 +0100

    help

---
+++ commands/nhelp.js
@@ -0,0 +1,17 @@
+const Booru = require('booru')
+const { MessageEmbed, MessageFlags } = require("discord.js")
+module.exports = {
+	name: 'xb',
+	description: 'ball',
+	execute(client, message, args) {
+        let embed = new MessageEmbed
+        .setTitle("NSFW HELP")
+        .setDescription(`!e621\n
+        !r34\n
+        !realb\n
+        !xb`)
+        .setFooter(`!e621 is furry\n
+        !r34 and !xb is rule34\n
+        !realb is real life`)
+	},
+};
