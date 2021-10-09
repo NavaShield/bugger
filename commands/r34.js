351ccb6d73913e0da7010db23623f7c395f4a8f3
ChenzIsHomo <88568085+ChenzIsHomo@users.noreply.github.com>
2021-10-09 17:19:55 +0100

    suffy wuffy owo?

---
+++ commands/r34.js
@@ -0,0 +1,17 @@
+const Booru = require('booru')
+const { MessageEmbed, MessageFlags } = require("discord.js")
+module.exports = {
+	name: 'r34',
+	description: 'ball',
+	execute(client, message, args) {
+        if (message.channel.nsfw === false) return message.channel.send('Please use this command in an nsfw channel.')
+        Booru.search('r34', [`${args[0]}`], { limit: 1, random: true })
+        .then(posts => {
+          for (let post of posts)
+          var embed = new MessageEmbed() 
+          .setTitle(`${args[0]}`)
+          .setImage(post.fileUrl)
+          message.channel.send(embed)
+        })
+	},
+};
