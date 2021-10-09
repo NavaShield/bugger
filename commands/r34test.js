8cfa0c5e331e541bb848acd5ca5e3a05601e61c6
ChenzIsHomo <88568085+ChenzIsHomo@users.noreply.github.com>
2021-10-09 16:30:00 +0100

    asdasd

--- commands/r34test.js
+++ commands/r34test.js
@@ -1,11 +1,11 @@
 const Discord = require('discord.js');
+const Booru = require('booru')
 module.exports = {
     name: 'r34test',
     execute(client, message, args){
         if (!message.guild) return;
         if (message.channel.nsfw === false) return message.channel.send('wee wee woo woo this aint a poo poo channel')
-        const { posts } = require('rule34js') // const posts = require("rule34js").posts
-        const response = await posts({tags:["furry"]})
-        console.log(response)
+        const sb = Booru.forSite('sb')
+        sb.search(['ball'], { limit: 2 })
     }
 }
