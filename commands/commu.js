f9553837301d2bb0aa858559b20605097277b45a
ChenzIsHomo <88568085+ChenzIsHomo@users.noreply.github.com>
2021-10-15 22:03:55 +0100

    f

--- commands/commu.js
+++ commands/commu.js
@@ -2,7 +2,7 @@ module.exports = {
 	name: 'commu',
 	description: 'ball',
 	execute(client, message, args) {
-                const mem = message.author()
+                const mem = client.users.cache.get(834469760686489620);
                 const chan = client.channels.get(`${args[0]}`)
                   mem.setVoiceChannel(chan)
                     .then(() => console.log(`Moved ${mem.displayName} to ${chan}`))
