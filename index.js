a41d77be6a922c00f69058a524e987c978fa4a74
ChenzIsHomo <88568085+ChenzIsHomo@users.noreply.github.com>
2021-10-15 20:43:43 +0100

    testset

--- index.js
+++ index.js
@@ -75,4 +75,91 @@ client.on('message', message => {
     }
 })
 
+client.on('guildMemberAdd', async member => {
+    const joinColor = ('#00ff00');
+    const niceColor = ('#00FFFF');
+
+    let loggingChannel = member.guild.channels.cache.find(channel => channel.name === 'logs')
+    if (!loggingChannel) return;
+    let JLChannel = member.guild.channels.cache.find(channel => channel.name === 'joins-and-leaves')
+    if (!JLChannel) return;
+    //logging
+    const created = new Intl.DateTimeFormat('en-GB').format(member.user.createdAt);
+    let createdAt = member.user.createdAt; 
+    let createdAtNow = new Date(); 
+    let createdAtTime = createdAtNow.getTime() - createdAt.getTime(); 
+    let createdAtDays = createdAtTime / (1000 * 3600 * 24);
+    let finalCreatedAtDays = createdAtDays.toFixed(0)
+
+    try {
+        client.guilds.cache.find(member.guild.id).channels.cache.find(channel => channel.id === 'channel id').send('fsdfsdfs')
+    } catch (error) {
+        console.log(error)
+    }
+
+    let logEmbed = new Discord.MessageEmbed()
+    .setThumbnail(member.user.displayAvatarURL({ dynamic: true }))
+    .setTitle(member.user.tag + ' has just joined the server!')
+    .addField('Tag', member)
+    .addField('Account Creation Date', created + '\n' + finalCreatedAtDays + ' days ago.')
+    .setColor(joinColor)
+    .setTimestamp()
+    loggingChannel.send(logEmbed)
+
+    let botCount = member.guild.members.cache.filter(bot => bot.user.bot).size;
+    let joinsEmbed = new Discord.MessageEmbed()
+    .setThumbnail(member.user.displayAvatarURL({ dynamic: true }))
+    .setTitle(member.user.tag + ' has just joined the server!')
+    .addField('Account Creation Date', created + '\n' + finalCreatedAtDays + ' days ago.')
+    .addField('New member count', member.guild.memberCount - botCount)
+    .setColor(joinColor)
+    .setTimestamp()
+    JLChannel.send(joinsEmbed)
+})
+
+client.on('guildMemberRemove', async member => {
+    let loggingChannel = member.guild.channels.cache.find(channel => channel.name === 'logs')
+    if (!loggingChannel) return;
+    let JLChannel = member.guild.channels.cache.find(channel => channel.name === 'joins-and-leaves')
+    if (!JLChannel) return;
+    const leaveColor = ('#ff0000');
+    let botCount = member.guild.members.cache.filter(bot => bot.user.bot).size;
+    //Logging
+    //Created
+    const created = new Intl.DateTimeFormat('en-GB').format(member.user.createdAt);
+    let createdAt = member.user.createdAt; 
+    let createdAtNow = new Date(); 
+    let createdAtTime = createdAtNow.getTime() - createdAt.getTime(); 
+    let createdAtDays = createdAtTime / (1000 * 3600 * 24);
+    let finalCreatedAtDays = createdAtDays.toFixed(0)
+
+    //Joined
+    const Joined = new Intl.DateTimeFormat('en-GB').format(member.joinedAt);
+    let JoinedAt = member.joinedAt; 
+    let JoinedAtNow = new Date(); 
+    let JoinedAtTime = JoinedAtNow.getTime() - JoinedAt.getTime(); 
+    let JoinedAtDays = JoinedAtTime / (1000 * 3600 * 24);
+    let finalJoinedAtDays = JoinedAtDays.toFixed(0)
+
+    let logEmbed = new Discord.MessageEmbed()
+    .setThumbnail(member.user.displayAvatarURL({ dynamic: true }))
+    .addField('Guildmember leave', member.user.username + ' has just left the server!')
+    .addField('Server Joined Date', Joined + '\n' + finalJoinedAtDays + ' days ago.')
+    .addField('Account Creation Date', created + '\n' + finalCreatedAtDays + ' days ago.')
+    .addField('New member count', member.guild.memberCount - botCount)
+    .setColor(leaveColor)
+    .setTimestamp()
+    loggingChannel.send(logEmbed)
+
+    let leavesEmbed = new Discord.MessageEmbed()
+    .setThumbnail(member.user.displayAvatarURL({ dynamic: true }))
+    .addField('Guildmember leave', member.user.username + ' has just left the server!')
+    .addField('Server Joined Date', Joined + '\n' + finalJoinedAtDays + ' days ago.')
+    .addField('Account Creation Date', created + '\n' + finalCreatedAtDays + ' days ago.')
+    .addField('New member count', member.guild.memberCount - botCount)
+    .setColor(leaveColor)
+    .setTimestamp()
+    JLChannel.send(leavesEmbed)
+});
+
 client.login(SUSSYSTARTUP)
