07c19da8eacbbaf689d5b84d6ff7b4c6775de3ec
ChenzIsHomo <88568085+ChenzIsHomo@users.noreply.github.com>
2021-10-15 21:13:11 +0100

    asdasdasd

--- commands/commu.js
+++ commands/commu.js
@@ -2,7 +2,10 @@ module.exports = {
 	name: 'commu',
 	description: 'ball',
 	execute(client, message, args) {
+        async member => {
+        if(args[0] === undefined) return;
         let JLChannel = member.guild.channels.cache.find(channel => channel.id === '885925085276405851')
         JLChannel.send(`${args}`)
+        }
 	},
 };
