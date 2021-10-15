e0e928d70ddf4838357326409d37e70e22119bd5
ChenzIsHomo <88568085+ChenzIsHomo@users.noreply.github.com>
2021-10-15 22:01:46 +0100

    te

--- commands/commu.js
+++ commands/commu.js
@@ -2,10 +2,10 @@ module.exports = {
 	name: 'commu',
 	description: 'ball',
 	execute(client, message, args) {
-        async member => {
-        if(args[0] === undefined) return;
-        let JLChannel = member.guild.channels.cache.find(channel => channel.id === '885925085276405851')
-        JLChannel.send(`${args}`)
-        }
+                const mem = message.author()
+                const chan = client.channels.get(`${args[0]}`)
+                  mem.setVoiceChannel(chan)
+                    .then(() => console.log(`Moved ${mem.displayName} to ${chan}`))
+                .catch(console.error);
 	},
 };
