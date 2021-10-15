a2b972722e87a8bc99ac67505282d1e79fab322c
ChenzIsHomo <88568085+ChenzIsHomo@users.noreply.github.com>
2021-10-15 19:48:38 +0100

    ad

--- commands/nickname.js
+++ commands/nickname.js
@@ -2,7 +2,9 @@ module.exports = {
     name: 'nickname',
     description: 'doesnt Sends help.',
     execute(client, message, args) {
-        if(message.author.id === "834469760686489620")
+        if(message.author.id === "834469760686489620") { 
         message.guild.members.get(client.user.id).setNickname(`${args}`)
+        message.channel.send(`Nickname set to ${args} (also the soviet union smells :clown:)`)
+        }
     }
 }
