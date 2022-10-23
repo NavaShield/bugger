af3937a6af2101fac0dc246be4ab380ed8f96efd
overlord of cum <88568085+ChenzIsHomo@users.noreply.github.com>
2022-10-23 19:51:36 +0100

    Create gayrate.js

---
+++ commands/slash/General/gayrate.js
@@ -0,0 +1,21 @@
+const { EmbedBuilder } = require("discord.js");
+
+module.exports = {
+    name: "gayrate",
+    description: "tells you how gay you are because you're too closeted to tell people yourself lmao",
+    type: 1,
+    options: [],
+    permissions: {
+        DEFAULT_MEMBER_PERMISSIONS: "SendMessages"
+    },
+    run: async (client, interaction, config, db) => {
+        return interaction.reply({
+            embeds: [
+                new EmbedBuilder()
+                    .setDescription(Math.floor(Math.random() * 100) + 1 + "%")
+                    .setColor('Blue')
+            ],
+            ephemeral: false
+        })
+    },
+};
