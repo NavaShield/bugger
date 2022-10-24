8b75866d4d8dd0653f29fbae12cf7ddfb63c4128
overlord of cum <88568085+ChenzIsHomo@users.noreply.github.com>
2022-10-24 16:03:35 +0100

    Create capybara.js

---
+++ commands/slash/General/capybara.js
@@ -0,0 +1,23 @@
+const { EmbedBuilder } = require("discord.js");
+
+module.exports = {
+    name: "cat",
+    description: "cat (not to be confused with kat)",
+    type: 1,
+    options: [],
+    permissions: {
+        DEFAULT_MEMBER_PERMISSIONS: "SendMessages"
+    },
+    run: async (client, interaction, config, db) => {
+        const response = await fetch('https://api.capybara-api.xyz/v1/image/random');
+        const data = await response.json();
+        return interaction.reply({
+            embeds: [
+                new EmbedBuilder()
+                    .setTitle("Random Capybara Image")
+                    .setImage(`${data.image_urls.original}`)
+            ],
+            ephemeral: false
+        })
+    },
+};
