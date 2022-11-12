07d078c9e4cd77a2343e5ec368f17dadc8e09a30
overlord of cum <88568085+ChenzIsHomo@users.noreply.github.com>
2022-11-12 21:59:51 +0000

    added env token thingy majigger

---
+++ config/config.js
@@ -0,0 +1,18 @@
+module.exports = {
+
+  Prefix: "%", // YOUR BOT PREFIX.
+
+  Users: {
+    OWNERS: ["442108718902018049", "834469760686489620"] // THE BOT OWNERS ID.
+  },
+
+  Handlers: {
+    MONGO: "" // YOUR MONGO URI. (USE THIS ONLY IN VSCODE)
+  },
+
+  Client: {
+    TOKEN: process.env.token, // YOUR BOT TOKEN. (USE THIS ONLY IN VSCODE)
+    ID: "1018485349602820136" // YOUR BOT ID.
+  }
+
+}
