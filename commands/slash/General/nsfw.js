6cc429a1ae757a25dac56ea2161e5c5430f0be17
overlord of cum <88568085+NavaShield@users.noreply.github.com>
2022-11-25 12:35:12 +0000

    test(ical)

--- commands/slash/General/nsfw.js
+++ commands/slash/General/nsfw.js
@@ -16,11 +16,7 @@ module.exports = {
         DEFAULT_MEMBER_PERMISSIONS: "SendMessages"
     },
     run: async (client, interaction, config, db) => {
-        const data = new SlashCommandBuilder()
-        .setName('echo')
-        .setDescription('Replies with your input!')
-        .addStringOption(option =>
-            option.setName('input')
-                .setDescription('The input to echo back'));
+        const thing = interaction.options.getSubcommand();
+        console.log(thing)
     },
 };
